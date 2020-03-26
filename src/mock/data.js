30d848d5da62e0e7b653fdf6215047bdf5f3c21b
Austin Denny <austinbdenny@gmail.com>
2020-03-26 14:44:23 -0500

    Updated site.

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,31 +18,36 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'techs.png',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne:
+    'Recent Grad. Passionate about learning, teaching, all things science & tech. My background in Biology has taught me many necessary skills of a successful developer. Working as a researcher and supplemental instructor, I learned the value of punctuality, communication, and the necessity of building meaningful rapport with coworkers. I have experience learning fast, and in turn, conveying complex ideas simply. My responsibilities often required digging deep into rigorous material under short time constraints.',
+  paragraphTwo:
+    'This experience opened my eyes to how outdated much of academia is becoming. I found myself using online resources, often with better outcomes. This brought about my budding interest in web development. The wide audience you can reach through the internet, irrespective of time and location, excited me. When I began learning about these technologies, I discovered a love and aptitude for them.',
+  paragraphThree:
+    "In fall 2019, I committed fully to pursuing web development. I attended a 16-week immersive full-stack coding boot camp at DigitalCrafts, in the PERN tech stack. I'm passionate about learning new technologies, broadening my knowledge of the tech industry, and making meaningful contributions to real-world products.",
+
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  // {
+  //   id: uuidv1(),
+  //   img: 'project.jpg',
+  //   title: '',
+  //   info: '',
+  //   info2: '',
+  //   url: '',
+  //   repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  // },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'tribuo.png',
+    title: 'Tribuo',
+    info:
+      'Tribuo is a full stack web app for company-wide task management that tracks employees, departments, and tasks in a relational database.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/abdenny/Tribuo', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
